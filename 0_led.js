2b61709925c28e375d9e62584a70c2b98dbcc3a0
Javier Abadia <javi.abadia@gmail.com>
2014-09-28 17:42:16 +0200

    first demo - simple led blinking

---
+++ 0_led.js
@@ -0,0 +1,13 @@
+"use strict";
+
+var five = require('johnny-five');
+
+var board = new five.Board();
+
+board.on('ready', function()
+{
+	console.log("board ready");
+
+	var led = new five.Led(13);	
+	led.blink();
+});
